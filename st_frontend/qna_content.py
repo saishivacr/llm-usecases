b085ded36882adb1d1173b5b409543f7d19ea371
SaiShiva Gudla <Saishiva.Gudla@criticalriver.com>
2023-08-03 17:36:09 +0530

    added session state to query input

--- st_frontend/qna_content.py
+++ st_frontend/qna_content.py
@@ -5,7 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-
+if 'db_loaded' not in st.session_state:
+    st.session_state.db_loaded = False
 
 
 # Get the absolute path to the project root directory
@@ -99,16 +100,19 @@ def qna_docs():
             st.exception(error_msg)
 
         if st.button("Digest Documents", disabled=not st.session_state.upload_docs_qna):
+            st.session_state.db_loaded = False
             with st.spinner("Reading contents of documents..."):
                 from vectorstore_db import run_db_build
                 db = run_db_build()
                 print(db)
-        if db:
-            st.success("Sucecssfully digested the content of the documents ✔️. You can proceed to interact with with your docuemnts.")
-            query = st.text_input(label="Ask queries from your documents",
-                            placeholder="Type your query....")
-            if len(query)>0:
-                st.write("Implementing logic to get completion.")
+            if db:
+                st.success("Sucecssfully digested the content of the documents ✔️. You can proceed to interact with with your docuemnts.")
+                st.session_state.db_loaded = True
+        query = st.text_input(label="Ask queries from your documents",
+                        placeholder="Type your query....",
+                        disabled=not st.session_state.db_loaded)
+        if len(query)>0:
+            st.write("Implementing logic to get completion.")
 
 
                 
